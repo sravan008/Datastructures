a0abdfac4f7874c440cc69f8a4a78e7766401040
sravan <findmewalle@gmail.com>
2017-11-27 00:53:59 +0530

    Initial commit

---
+++ src/org/ksk/ds/DoubleLinkedList.java
@@ -0,0 +1,25 @@
+package org.ksk.ds;
+
+/**
+ * Created by sravan on 11/27/2017.
+ */
+public class DoubleLinkedList {
+
+
+    public DListNode insertNode(DListNode headNode, DListNode nodeToinsert,int poistion){
+
+       /* if (headNode == null) return headNode;
+        //int size = getLength(headNode);
+        if (poistion > size + 1 || poistion < 1) {
+            System.out.println("Please enter the correct combination");
+            return headNode;
+        }*/
+    return null;
+
+
+
+
+
+    }
+
+}
